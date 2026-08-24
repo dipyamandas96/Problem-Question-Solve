af01e798b48af96abce966918ba1469d388ca240
DIPYAMAN DAS <89519912+dipyamandas96@users.noreply.github.com>
2026-08-24 13:50:21 +0530

    Time: 630 ms (97.4%), Space: 34 MB (67.95%) - LeetHub

---
+++ 0327-count-of-range-sum/0327-count-of-range-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
+        psum = [0]
+        for num in nums:
+            psum.append(psum[-1] + num)
+
+        result, slist = 0, SortedList()
+        for pval in reversed(psum):
+            result += slist.bisect_right(pval + upper) - slist.bisect_left(pval + lower)
+            slist.add(pval)
+
+        return result
