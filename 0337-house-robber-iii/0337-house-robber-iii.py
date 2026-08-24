b786c25d8a190c451b5f4f9b6890473bed46b6b3
DIPYAMAN DAS <89519912+dipyamandas96@users.noreply.github.com>
2026-08-24 13:56:46 +0530

    Time: 6 ms (25.59%), Space: 20.7 MB (70.35%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -0,0 +1,12 @@
+
+
+class Solution:
+    def rob(self, root: TreeNode) -> int:
+        return max(self.dfs(root))
+    
+    def dfs(self, root: TreeNode):
+        if not root:
+            return (0, 0)
+        left = self.dfs(root.left)
+        right = self.dfs(root.right)
+        return (root.val + left[1] + right[1], max(left[0], left[1]) + max(right[0], right[1]))
